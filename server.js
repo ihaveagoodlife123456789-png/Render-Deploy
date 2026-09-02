1ffff09e6ceee1cc520b924a39f754f6e8137978
ihaveagoodlife123456789-png <ihaveagoodlife123456789@gmail.com>
2026-09-01 20:58:17 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,19 @@ app.get('/', async (req, res) => {
     try {
         const result = await pool.query('SELECT * FROM users');
 
-        if(!result.ok || result.rows.length === 0) {
-            throw new Error('Failed to fetch users');
+        if(result.rows.length === 0) {
+            return res.status(404).send('Failed to fetch users');
         }
+        
         res.send(result.rows);
         console.log(result.rows);
     } catch (error) {
-        res.status(500).send(error.message);
+        res.status(500).send(error.message); 
     }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, '0.0.0.0',  () => {
+    console.log(`Server is running on port ${PORT}`);
 });
