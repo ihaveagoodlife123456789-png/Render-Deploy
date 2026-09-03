513322a247d14f58fc3553ac10bb60229dcf24f7
ihaveagoodlife123456789-png <ihaveagoodlife123456789@gmail.com>
2026-09-02 20:05:58 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,6 +21,8 @@ app.get('/', async (req, res) => {
     }
 });
 
+const PORT = process.env.PORT || 3000; 
+
 app.listen(PORT, '0.0.0.0',  () => {
     console.log(`Server is running on port ${PORT}`);
 });
